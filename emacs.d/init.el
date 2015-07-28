0d08734400c6d34a0e201257a7dffa889476828c
Sebastián Estrella <sebas007estrella@gmail.com>
2015-07-28 15:06:07 -0500

    [emacs] Show line numbers

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,15 @@
+;; show line numbers
+(setq linum-format "%4d \u2502 ")
+(global-linum-mode 1)
+
+;; follow symlinks without prompting
+(setq vc-follow-symlinks t)
+
 (setq auto-mode-alist (cons '("Cask" . emacs-lisp-mode) auto-mode-alist))
 
+;; load packages using cask
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
+;; enable evil-mode by default
 (evil-mode 1)
