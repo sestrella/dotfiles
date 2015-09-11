10b33f320562781a76a557b6980b4e17589b0b53
Sebastián Estrella <sebas007estrella@gmail.com>
2015-09-11 00:38:06 -0500

    [zsh] Fix issues related to sh files

--- zsh/nvm.sh
+++ zsh/nvm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 if [ -e "$HOME/.nvm/nvm.sh" ]; then
-    source "$HOME/.nvm/nvm.sh"
+  source "$HOME/.nvm/nvm.sh"
 fi
-alias npm-exec="PATH=$(npm bin):$PATH"
+
+if which npm > /dev/null; then
+  alias npm-exec="PATH=$(npm bin):$PATH"
+fi
