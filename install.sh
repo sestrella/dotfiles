6412bd7c6ffc51b04bed931898849ef2922a0beb
Sebastián Estrella <sebas007estrella@gmail.com>
2015-09-26 17:32:43 -0500

    Add installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+ln -sf $PWD/bundle/config $HOME/.bundle/config
+ln -sf $PWD/emacs.d/Cask $HOME/.emacs.d/Cask
+ln -sf $PWD/emacs.d/init.el $HOME/.emacs.d/init.el
+ln -sf $PWD/gitconfig $HOME/.gitconfig
+ln -sf $PWD/tmux.conf $HOME/.tmux.conf
+ln -sf $PWD/zpreztorc $HOME/.zpreztorc
+ln -sf $PWD/zshrc $HOME/.zshrc
