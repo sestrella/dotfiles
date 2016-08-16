a87f4e3e66661fe2e80e1f5fd9393ce7dcdab79d
Sebastián Estrella <sebas007estrella@gmail.com>
2016-08-16 00:11:48 -0500

    Emacs install solarized theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,4 +4,13 @@
 ;(autoload 'my-site-start "~/.emacs.d/my-site-start/my-site-start" nil t)
 ;(my-site-start "~/.emacs.d/site-start.d")
 
+(menu-bar-mode -1)
+
+(setq solarized-termcolors 256)
+(set-terminal-parameter nil 'background-mode 'dark)
+(load-theme 'solarized t)
+
+(require 'evil)
+(evil-mode 1)
+
 (provide 'init)
