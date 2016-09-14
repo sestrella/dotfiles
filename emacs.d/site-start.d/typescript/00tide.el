a021cffae9f46a5f0d37c2e303cb2796bc9766bf
Sebastián Estrella <sebas007estrella@gmail.com>
2016-09-14 08:23:46 -0500

    [emacs] Add keybinding for TypeScript

--- emacs.d/site-start.d/typescript/00tide.el
+++ emacs.d/site-start.d/typescript/00tide.el
@@ -1,2 +1,6 @@
 (require 'tide)
 (add-hook 'typescript-mode-hook 'tide-setup)
+
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c r") 'tide-rename-symbol)))
