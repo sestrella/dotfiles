333f4cc5eaf2cbdef2d6329b894e88962854c1c1
Sebastián Estrella <sebas007estrella@gmail.com>
2017-01-20 23:31:43 -0500

    [zsh] Initialize rbenv

--- zsh/init-nvm.sh
+++ zsh/init-nvm.sh
@@ -1,2 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+if [[ -s "$NVM_DIR/nvm.sh" ]]; then
+  source "$NVM_DIR/nvm.sh"
+fi
---
+++ zsh/init-rbenv.sh
@@ -0,0 +1,5 @@
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+if [[ -s "$HOME/.rbenv/bin/rbenv" ]]; then
+  eval "$(rbenv init -)"
+fi
