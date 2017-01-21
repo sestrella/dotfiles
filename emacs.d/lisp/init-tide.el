0cd762dc3ccf2044fa186b25992676799e0539f7
Sebastián Estrella <sebas007estrella@gmail.com>
2017-01-21 00:16:46 -0500

    [emacs] Bind typescript symbols

--- emacs.d/lisp/init-tide.el
+++ emacs.d/lisp/init-tide.el
@@ -1,5 +1,10 @@
 (use-package tide
-  :config (add-hook 'typescript-mode-hook #'tide-setup)
+  :config
+  (progn
+    (add-hook 'typescript-mode-hook #'tide-setup)
+
+    (eval-after-load 'evil-leader
+      (evil-leader/set-key-for-mode 'typescript-mode "r" 'tide-rename-symbol)))
   :ensure t)
 
 (provide 'init-tide)
