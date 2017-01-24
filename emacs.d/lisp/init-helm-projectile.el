7e4c20aae81e618ed1421afea77b38f2a645c656
Sebastián Estrella <sebas007estrella@gmail.com>
2017-01-24 03:12:37 -0500

    [emacs] Add custom function to toggle between test and source files

--- emacs.d/lisp/init-helm-projectile.el
+++ emacs.d/lisp/init-helm-projectile.el
@@ -1,7 +1,18 @@
 (use-package helm-projectile
+  :init
+  (defun custom/projectile-test-suffix-function (project-type)
+    "Find default test files suffix based on PROJECT-TYPE."
+    (if (eq project-type 'node)
+	    ".test"
+      (projectile-test-suffix-function project-type)))
+
+  (setq projectile-test-suffix-function #'custom/projectile-test-suffix-function)
   :config
+  (projectile-register-project-type 'node '("package.json") nil "npm test")
   (eval-after-load 'evil-leader
-    (evil-leader/set-key "f" 'helm-projectile-find-file))
+    (evil-leader/set-key
+      "f" 'helm-projectile-find-file
+      "t" 'projectile-toggle-between-implementation-and-test))
   :ensure t)
 
 (provide 'init-helm-projectile)
