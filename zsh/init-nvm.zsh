399c12dce182f0f70f0a7f86cd7e7989b8e2b39e
Sebastián Estrella <sebas007estrella@gmail.com>
2017-02-19 01:28:27 -0500

    Source nvm bash completion

--- zsh/init-nvm.zsh
+++ zsh/init-nvm.zsh
@@ -2,7 +2,13 @@ export NVM_DIR="$HOME/.nvm"
 
 if [[ -s "$NVM_DIR/nvm.sh" ]]; then
   source "$NVM_DIR/nvm.sh"
+fi
 
+if [[ -s "$NVM_DIR/bash_completion" ]]; then
+  source "$NVM_DIR/bash_completion"
+fi
+
+if type nvm > /dev/null; then
   autoload -U add-zsh-hook
 
   load-nvmrc() {
