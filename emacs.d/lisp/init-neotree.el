40cd87c702eba91130f2d0699e2776da82236ee3
Sebastián Estrella <sebas007estrella@gmail.com>
2017-03-09 00:07:29 -0500

    [emacs] Display message while refreshing neotree

--- emacs.d/lisp/init-neotree.el
+++ emacs.d/lisp/init-neotree.el
@@ -2,11 +2,17 @@
   :init
   (setq neo-show-hidden-files t)
   :config
-  (add-hook 'neotree-mode-hook
-    (lambda ()
-      (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+  (with-eval-after-load "evil"
+    (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+    (evil-define-key 'normal neotree-mode-map (kbd "r") 'custom/neotree-refresh))
 
   (with-eval-after-load "evil-leader"
     (evil-leader/set-key "n" 'neotree-toggle)))
+
+(defun custom/neotree-refresh ()
+  "Refresh the NeoTree buffer."
+  (interactive)
+  (message "NeoTree: Refreshing node. This could take a while... DONE")
+  (neotree-refresh))
 
 (provide 'init-neotree)
