26fe7eb9305704605d39f318e623e71eff0343aa
Sebastián Estrella <sebas007estrella@gmail.com>
2017-03-09 01:22:30 -0500

    [emacs] Add tide keybindings

--- emacs.d/lisp/init-tide.el
+++ emacs.d/lisp/init-tide.el
@@ -1,5 +1,10 @@
 (use-package tide
   :config
+  (with-eval-after-load "evil-leader"
+    (evil-leader/set-key-for-mode 'typescript-mode "tb" 'tide-jump-back)
+    (evil-leader/set-key-for-mode 'typescript-mode "td" 'tide-jump-to-definition)
+    (evil-leader/set-key-for-mode 'typescript-mode "tr" 'tide-rename-symbol))
+
   (add-hook 'typescript-mode-hook 'tide-setup))
 
 (provide 'init-tide)
