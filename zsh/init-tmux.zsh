b2f87155e78fb676c63bf832ffb34046fcde4881
Sebastián Estrella <sebas007estrella@gmail.com>
2017-03-09 01:06:28 -0500

    [zsh] Update tmux alias

--- zsh/init-tmux.zsh
+++ zsh/init-tmux.zsh
@@ -1,4 +1,4 @@
-alias tas="tmux attach-session -t"
-alias tks="tmux kill-session -t"
-alias tls="tmux list-sessions"
-alias tns="tmux new-session -s"
+alias ta="tmux attach-session -t"
+alias tk="tmux kill-session -t"
+alias tl="tmux list-sessions"
+alias tn="tmux new-session -s"
