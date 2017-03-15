cfcedda5cfda34cffb01e9c9f5efc7a8219702b1
Sebastián Estrella <sebas007estrella@gmail.com>
2017-03-15 01:04:27 -0500

    [emacs] Open neotree on root dir

--- emacs.d/lisp/init-neotree.el
+++ emacs.d/lisp/init-neotree.el
@@ -12,12 +12,25 @@
     (evil-define-key 'normal neotree-mode-map (kbd "r") 'custom/neotree-refresh))
 
   (with-eval-after-load "evil-leader"
-    (evil-leader/set-key "n" 'neotree-toggle)))
+    (evil-leader/set-key "n" 'custom/neotree-toggle)))
 
 (defun custom/neotree-refresh ()
   "Refresh the NeoTree buffer."
   (interactive)
   (message "NeoTree: Refreshing node. This could take a while... DONE")
   (neotree-refresh))
+
+(defun custom/neotree-toggle ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+	(file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+	(if (neo-global--window-exists-p)
+	    (progn
+	      (neotree-dir project-dir)
+	      (neotree-find file-name)))
+      (message "Could not find git project root."))))
 
 (provide 'init-neotree)
