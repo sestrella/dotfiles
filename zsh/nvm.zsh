009cd3413551bc66a73243bbcac1cb982c4ed09c
Sebastián Estrella <sebas007estrella@gmail.com>
2017-04-26 01:15:16 -0500

    [zsh] Update nvm init script

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,9 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
-
-if [[ -s "$NVM_DIR/nvm.sh" ]]; then
-  source "$NVM_DIR/nvm.sh"
-fi
-
-if [[ -s "$NVM_DIR/bash_completion" ]]; then
-  source "$NVM_DIR/bash_completion"
+if [[ -s /usr/share/nvm/init-nvm.sh ]]; then
+  source /usr/share/nvm/init-nvm.sh
 fi
