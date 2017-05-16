cde98e43af77125078cb5c07df06ba0721d25f5f
Sebastián Estrella <sebas007estrella@gmail.com>
2017-05-16 07:08:14 -0500

    Add systemd alias

---
+++ zsh/systemd.zsh
@@ -0,0 +1,7 @@
+alias syp='systemctl stop'
+alias syr='systemctl restart'
+alias sys='systemctl start'
+alias syt='systemctl status'
+
+alias sylt='systemctl list-timers'
+alias sylu='systemctl list-units'
