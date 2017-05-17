3dbcdfc3ed21c5c6632ba66a843cffd53dae727e
Sebastián Estrella <sebas007estrella@gmail.com>
2017-05-17 16:05:27 -0500

    Fix nvm and add more systemctl alias

--- zsh/systemd.zsh
+++ zsh/systemd.zsh
@@ -2,6 +2,7 @@ alias syp='systemctl stop'
 alias syr='systemctl restart'
 alias sys='systemctl start'
 alias syt='systemctl status'
+alias syu='systemctl --user'
 
 alias sylt='systemctl list-timers'
 alias sylu='systemctl list-units'
