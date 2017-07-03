3356cc6853473a834a9ceb5e9f1948d51afe8e3a
Sebastián Estrella <sebas007estrella@gmail.com>
2017-07-03 08:49:45 -0500

    [emacs] Install Haskell packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,13 @@
 (setq custom-file "~/.emacs.d/custom.el")
 
+(menu-bar-mode -1)
+
+(setq linum-format "%d ")
+(global-linum-mode)
+
+(custom-set-variables
+  '(haskell-stylish-on-save t))
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
@@ -11,4 +19,16 @@
 
 (use-package evil
   :config (evil-mode 1)
+  :ensure t)
+
+(use-package flycheck
+  :config (global-flycheck-mode)
+  :ensure t)
+
+(use-package company
+  :config (add-hook 'after-init-hook 'global-company-mode)
+  :ensure t)
+
+(use-package intero
+  :config (add-hook 'haskell-mode-hook 'intero-mode)
   :ensure t)
