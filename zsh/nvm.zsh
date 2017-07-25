c3e9736b2b3c39794b767eaf2151d1aceed5e574
Sebastián Estrella <sebas007estrella@gmail.com>
2017-07-24 23:13:20 -0500

    Install nvm locally

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,3 +1,9 @@
-if [[ -s /usr/share/nvm/init-nvm.sh ]]; then
-  source /usr/share/nvm/init-nvm.sh
+export NVM_DIR="$HOME/.nvm"
+
+if [[ -s "$NVM_DIR/nvm.sh" ]]; then
+  source "$NVM_DIR/nvm.sh"
+fi
+
+if [[ -s "$NVM_DIR/bash_completion" ]]; then
+  source "$NVM_DIR/bash_completion"
 fi
