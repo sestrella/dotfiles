7a78fb146adb206622b7b8e9affb8ef1cdb514d9
Sebastián Estrella <sebas007estrella@gmail.com>
2017-07-27 20:46:13 -0500

    Refactor zsh init scripts

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,9 +1,4 @@
 export NVM_DIR="$HOME/.nvm"
 
-if [[ -s "$NVM_DIR/nvm.sh" ]]; then
-  source "$NVM_DIR/nvm.sh"
-fi
-
-if [[ -s "$NVM_DIR/bash_completion" ]]; then
-  source "$NVM_DIR/bash_completion"
-fi
+[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/bash_completion" ]] && source "$NVM_DIR/bash_completion"
--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,4 +1,6 @@
-alias ta='tmux attach-session -t'
-alias tk='tmux kill-session -t'
-alias tl='tmux list-sessions'
-alias tn='tmux new-session -s'
+if type tmux > /dev/null; then
+  alias ta='tmux attach-session -t'
+  alias tk='tmux kill-session -t'
+  alias tl='tmux list-sessions'
+  alias tn='tmux new-session -s'
+fi
