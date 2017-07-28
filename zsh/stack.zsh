f8e6e8e19ec46d292f87eec7c1b22db7a3397969
Sebastián Estrella <sebas007estrella@gmail.com>
2017-07-27 20:40:07 -0500

    Add stack exec moo-postgresql alias

--- zsh/stack.zsh
+++ zsh/stack.zsh
@@ -5,6 +5,7 @@ if type stack > /dev/null; then
   alias sc='stack clean'
   alias se='stack exec'
   alias si='stack install'
+  alias sm='stack exec moo-postgresql'
   alias sr='stack repl'
   alias st='stack test'
 fi
