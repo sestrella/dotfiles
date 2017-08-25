5da60df35a8912cac7fc7e0c1e8afd7bce5cd1c4
Sebastián Estrella <sebas007estrella@gmail.com>
2017-08-25 00:10:15 -0500

    Refactor nvm init script

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,4 +1,9 @@
 export NVM_DIR="$HOME/.nvm"
 
-[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
-[[ -s "$NVM_DIR/bash_completion" ]] && source "$NVM_DIR/bash_completion"
+if [[ -s "$NVM_DIR/nvm.sh" ]] then;
+  source "$NVM_DIR/nvm.sh"
+fi
+
+if [[ -s "$NVM_DIR/bash_completion" ]] then;
+  source "$NVM_DIR/bash_completion"
+fi
