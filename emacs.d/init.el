b6e243a9463d206199c8859c037c08868280aab5
Sebastián Estrella <sebas007estrella@gmail.com>
2017-09-10 23:13:26 -0500

    [emacs] Refactor init script

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,48 +1,54 @@
-(setq package-list '())
-(add-to-list 'package-list 'company)
-(add-to-list 'package-list 'evil)
-(add-to-list 'package-list 'evil-leader)
-(add-to-list 'package-list 'fiplr)
-(add-to-list 'package-list 'flycheck)
-(add-to-list 'package-list 'intero)
-;;(add-to-list 'package-list 'psc-ide)
-;;(add-to-list 'package-list 'purescript-mode)
-
 (setq package-archives '())
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 
+(defun install-missing-packages (&rest packages)
+  (mapcar 'install-missing-package packages))
+
+(defun install-missing-package (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (unless package-archive-contents
   (package-refresh-contents))
 
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+(install-missing-packages
+  'company
+  'evil
+  'evil-leader
+  'fiplr
+  'flycheck
+  'intero
+  'neotree
+  'which-key)
+
+(global-set-key [escape] 'keyboard-escape-quit)
+(menu-bar-mode -1)
+
+(require 'linum)
+(setq linum-format "%d ")
+(global-linum-mode)
+
+(require 'which-key)
+(which-key-mode)
 
 (require 'evil)
 (evil-mode 1)
 
-(tool-bar-mode -1)
-
+(require 'evil-leader)
+(evil-leader/set-leader ",")
 (global-evil-leader-mode)
 
-(global-set-key [escape] 'keyboard-escape-quit)
-
-(evil-leader/set-leader ",")
+(require 'fiplr)
 (evil-leader/set-key "p" 'fiplr-find-file)
 
-;;(require 'purescript-mode)
-;;(require 'psc-ide)
-;;(add-hook 'purescript-mode-hook
-;;  (lambda ()
-;;    (psc-ide-mode)
-;;    (company-mode)
-;;    (flycheck-mode)))
+(require 'neotree)
+(evil-leader/set-key "n" 'neotree-toggle)
 
 (custom-set-variables
-   '(haskell-stylish-on-save t))
+  '(haskell-stylish-on-save (executable-find "stylish-haskell")))
 
 (require 'intero)
 (add-hook 'haskell-mode-hook 'intero-mode)
