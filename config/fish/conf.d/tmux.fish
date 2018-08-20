500e76d0882c326140c44e019f355249c8aeebcd
Sebastián Estrella <sestrella.me@gmail.com>
2018-08-19 21:53:05 -0500

    Add fish config.d init scripts

---
+++ config/fish/conf.d/tmux.fish
@@ -0,0 +1,6 @@
+alias ta='tmux attach -t'
+alias tad='tmux attach -d -t'
+alias tkss='tmux kill-session -t'
+alias tksv='tmux kill-server'
+alias tl='tmux list-sessions'
+alias ts='tmux new-session -s'
