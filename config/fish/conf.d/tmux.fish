f3ba055f95cedb143812b524e3b6d06df2df1be6
Sebastián Estrella <sestrella.me@gmail.com>
2018-10-17 12:26:10 -0500

    Fix tmux new session abbr

--- config/fish/conf.d/tmux.fish
+++ config/fish/conf.d/tmux.fish
@@ -2,4 +2,4 @@ abbr -a ta tmux attach -t
 abbr -a tkss tmux kill-session -t
 abbr -a tksv tmux kill-server
 abbr -a tl tmux list-sessions
-abbr -a ts tmux attach-session -t
+abbr -a ts tmux new-session -s
