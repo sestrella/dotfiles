413713c552da89c48022e83820ae6c91017c527b
Sebastián Estrella <sestrella.me@gmail.com>
2018-11-21 21:42:31 -0500

    Add more git abbrs

--- config/fish/conf.d/git.fish
+++ config/fish/conf.d/git.fish
@@ -1,1 +1,8 @@
+abbr -a ga git add
+abbr -a gaa git add --all
+abbr -a gb git branch
+abbr -a gc git commit -v
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gp git push
 abbr -a gst git status
