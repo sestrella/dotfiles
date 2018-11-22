fb8865a9c6f37edb7616f5d71f8333b8ca508a56
Sebastián Estrella <sestrella.me@gmail.com>
2018-11-21 21:54:29 -0500

    Add tmux abbrs

---
+++ config/fish/conf.d/tmux.fish
@@ -0,0 +1,6 @@
+abbr -a ta tmux attach -t
+abbr -a tad tmux attach -d -t
+abbr -a tkss tmux kill-session -t
+abbr -a tksv tmux kill-server
+abbr -a tl tmux list-sessions
+abbr -a ts tmux new-session -s
