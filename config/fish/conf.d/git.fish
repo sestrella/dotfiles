4eec3b35b7832ff2ace69fa3ba3c8350322c475e
Sebastián Estrella <sestrella.me@gmail.com>
2019-09-01 02:16:40 -0500

    [fish] Add gco abbr

--- config/fish/conf.d/git.fish
+++ config/fish/conf.d/git.fish
@@ -3,6 +3,7 @@ abbr gaa git add --all
 abbr gbr git branch --remote
 abbr gc git commit -v
 abbr gc! git commit -v --amend
+abbr gco git checkout
 abbr gd git diff
 abbr gl git pull
 abbr gp git push
