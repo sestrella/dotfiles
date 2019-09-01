1aa2934bb7b7b1e1988961424b3f6f022ced8d33
Sebastián Estrella <sestrella.me@gmail.com>
2019-09-01 01:33:01 -0500

    [fish] Add git abbreviations

---
+++ config/fish/conf.d/git.fish
@@ -0,0 +1,9 @@
+abbr ga git add
+abbr gaa git add --all
+abbr gbr git branch --remote
+abbr gc git commit -v
+abbr gc! git commit -v --amend
+abbr gd git diff
+abbr gl git pull
+abbr gp git push
+abbr gst git status
