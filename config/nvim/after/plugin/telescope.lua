8866c8352074e5827ec83a52e9e23da9f8e3a564
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-01 09:43:14 -0500

    Add keymap for telescope keymaps

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -2,4 +2,5 @@ local telescope = require("telescope")
 telescope.load_extension("fzf")
 
 local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<c-h>", builtin.keymaps, {})
 vim.keymap.set("n", "<c-p>", builtin.git_files, {})
