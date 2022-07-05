c4cfa940361eccffd99e27c6575d1e0b8c02a796
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-04 23:47:05 -0500

    Extract servers config to a different file

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -64,44 +64,7 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 -- luacheck: ignore
-local servers = {
-  ansiblels = {},
-  bashls = {},
-  dockerls = {
-    cmd = {
-      "env",
-      "ASDF_NODEJS_VERSION=18.4.0",
-      "docker-langserver",
-      "--stdio",
-    },
-  },
-  hls = {},
-  rust_analyzer = {},
-  sumneko_lua = {
-    settings = {
-      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-      Lua = {
-        runtime = { version = "LuaJIT", path = runtime_path },
-        diagnostics = { globals = { "vim" } },
-        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-        telemetry = { enable = false },
-      },
-    },
-  },
-  terraformls = {},
-  yamlls = {
-    settings = {
-      yaml = {
-        schemas = {
-          ["https://json.schemastore.org/circleciconfig.json"] = "/.circleci/config.yml",
-          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*.yml",
-          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-lint.json"] = "/.ansible-lint",
-          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/docker-compose*.yml",
-        },
-      },
-    },
-  },
-}
+local servers = require("lsp.servers")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
