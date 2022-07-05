47a4043141fc973d559b99f774f73a907e5e2025
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-04 23:19:02 -0500

    Set mapleader and maplocalleader

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,6 +14,9 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+vim.g.mapleader = "<space>"
+vim.g.maplocalleader = "<space>"
+
 require("plugins")
 
 -- screen
