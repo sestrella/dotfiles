0b6c4082d4fe10b946c5353abd79d591a4a38a8e
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-26 23:05:45 -0500

    Enable incremental_selection

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -6,5 +6,14 @@ require("nvim-treesitter.configs").setup({
   auto_install = true,
   highlight = {
     enable = true
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
   }
 })
