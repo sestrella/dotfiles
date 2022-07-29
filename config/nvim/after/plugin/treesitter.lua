156e6cd591e84c7a8f4cc397e4e5dd18c4807783
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-07-29 14:37:16 -0500

    Install textobjects

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -15,5 +15,28 @@ require("nvim-treesitter.configs").setup({
       scope_incremental = "grc",
       node_decremental = "grm",
     },
+  },
+  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects#text-objects-move
+  textobjects = {
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
   }
 })
