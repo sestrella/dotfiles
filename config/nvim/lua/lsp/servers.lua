ae24de889c43612838c23263ad84bf22230999da
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-08-05 13:28:11 -0500

    Check if executable is present before adding a server to lspconfig

--- config/nvim/lua/lsp/servers.lua
+++ config/nvim/lua/lsp/servers.lua
@@ -2,42 +2,48 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
--- luacheck: ignore
-return {
-  ansiblels = {},
-  bashls = {},
-  dockerls = {
-    cmd = {
-      "env",
-      "ASDF_NODEJS_VERSION=18.4.0",
-      "docker-langserver",
-      "--stdio",
+-- TODO: rename function
+local foo = function(cmd, name, value)
+  if vim.fn.executable(cmd) == 1 then
+    return { [name] = value }
+  end
+  vim.notify(string.format("%s not found", cmd), vim.log.levels.WARN)
+  return {}
+end
+
+local ansiblels = foo("ansible-language-server", "ansiblels", {})
+local bashls = foo("bash-language-server", "bashls", {})
+local rust_analyzer = foo("rust-analyzer", "rust_analyzer", {})
+local sumneko_lua = foo("lua-language-server", "sumneko_lua", {
+  settings = {
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+    Lua = {
+      runtime = { version = "LuaJIT", path = runtime_path },
+      diagnostics = { globals = { "vim" } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+      telemetry = { enable = false },
     },
-  },
-  hls = {},
-  rust_analyzer = {},
-  sumneko_lua = {
-    settings = {
-      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-      Lua = {
-        runtime = { version = "LuaJIT", path = runtime_path },
-        diagnostics = { globals = { "vim" } },
-        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-        telemetry = { enable = false },
+  }
+})
+local terraformls = foo("terraform-ls", "terraformls", {})
+local yamlls = foo("yaml-language-server", "yamlls", {
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/circleciconfig.json"] = "/.circleci/config.yml",
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*.yml",
+        ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-lint.json"] = "/.ansible-lint",
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/docker-compose*.yml",
       },
     },
   },
-  terraformls = {},
-  yamlls = {
-    settings = {
-      yaml = {
-        schemas = {
-          ["https://json.schemastore.org/circleciconfig.json"] = "/.circleci/config.yml",
-          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*.yml",
-          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-lint.json"] = "/.ansible-lint",
-          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/docker-compose*.yml",
-        },
-      },
-    },
-  },
-}
+})
+
+return vim.tbl_extend("error",
+  ansiblels,
+  bashls,
+  rust_analyzer,
+  sumneko_lua,
+  terraformls,
+  yamlls
+)
