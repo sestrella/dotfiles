8d7faf12d80abdf8113450db75a1bfa8849fa4d8
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-08-24 11:35:35 -0500

    Update treesitter settings

--- config/nvim/after/plugin/treesitter-context.lua
+++
@@ -1,6 +0,0 @@
-local ok, treesitter_context = pcall(require, "treesitter-context")
-if ok then
-  treesitter_context.setup({ enable = true })
-else
-  vim.notify("treesitter-context not found", vim.log.levels.WARN)
-end
--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -3,56 +3,7 @@ if not pcall(require, "nvim-treesitter") then
   return
 end
 
-local refactor_config = function()
-  if pcall(require, "nvim-treesitter-refactor") then
-    return {
-      refactor = {
-        highlight_current_scope = {
-          enable = true
-        },
-        highlight_definitions = {
-          enable = true
-        }
-      }
-    }
-  end
-  vim.notify("nvim-treesitter-refactor not found", vim.log.levels.WARN)
-  return {}
-end
-
-local textobjects_config = function()
-  if pcall(require, "nvim-treesitter-textobjects") then
-    -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects#text-objects-move
-    return {
-      textobjects = {
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-        },
-      }
-    }
-  end
-  vim.notify("nvim-treesitter-textobjects not found", vim.log.levels.WARN)
-  return {}
-end
-
-local default_config = {
+require("nvim-treesitter.configs").setup({
   auto_install = true,
   highlight = {
     enable = true
@@ -66,10 +17,4 @@ local default_config = {
       node_decremental = "grm",
     },
   }
-}
-
-require("nvim-treesitter.configs").setup(vim.tbl_extend("error",
-  default_config,
-  refactor_config(),
-  textobjects_config()
-))
+})
