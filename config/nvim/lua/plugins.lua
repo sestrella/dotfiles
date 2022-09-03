0369c13897231e586fecf3e92343d11560fe8d33
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-09-03 12:16:44 -0500

    Install asdf plugins

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,3 +1,9 @@
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
 
@@ -13,4 +19,8 @@ return require("packer").startup(function(use)
     requires = { "nvim-lua/plenary.nvim" }
   }
   use("nvim-treesitter/nvim-treesitter")
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
