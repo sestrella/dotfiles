d7876e3612b8c9f300098f182a870e10acba3ce5
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-09-04 12:45:41 -0500

    Refactor packer bootstrap

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,8 +1,14 @@
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
