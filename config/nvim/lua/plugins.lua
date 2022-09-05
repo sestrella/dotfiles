782842013e73314ff7cd248172571eec01af57e8
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-09-05 13:26:49 -0500

    Reorder plugins

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -18,16 +18,17 @@ return require("packer").startup(function(use)
 	use("hrsh7th/cmp-vsnip")
 	use("hrsh7th/nvim-cmp")
 	use("hrsh7th/vim-vsnip")
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+
 	use("neovim/nvim-lspconfig")
 	use({
 		"nvim-telescope/telescope.nvim",
 		requires = { "nvim-lua/plenary.nvim" },
 	})
 	use("nvim-treesitter/nvim-treesitter")
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
 
 	if packer_bootstrap then
 		require("packer").sync()
