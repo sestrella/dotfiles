f551b75a044a4b3747937582b66cee12980d76fb
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-09-06 01:55:07 -0500

    Remove telescope pcall check

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,4 @@
-if not pcall(require, "telescope") then
-  vim.notify("telescope not found", vim.log.levels.WARN)
-  return
-end
-
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<c-h>", builtin.keymaps, {})
+-- TODO: Check if .git directory exists or default to list_files
 vim.keymap.set("n", "<c-p>", builtin.git_files, {})
