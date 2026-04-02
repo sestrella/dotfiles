dfd25940d78180e4f3619034f493fc99647ceb39
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-04-02 16:45:03 -0500

    Delete old files

--- config/nvim/after/plugin/telescope.lua
+++
@@ -1,4 +0,0 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<c-h>", builtin.keymaps, {})
--- TODO: Check if .git directory exists or default to list_files
-vim.keymap.set("n", "<c-p>", builtin.git_files, {})
